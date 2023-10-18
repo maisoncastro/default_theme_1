d3560d3e063950735072fe62078b34171bae35d5
Nazim-Yoldas Sesen <nazimsesen@gmail.com>
2023-10-18 18:24:56 -0400

    Making Navbar, set perimeters for body

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,15 @@
+function NavBar() {
+  return (
+    <>
+      <div className="page-padding">
+        <div className="Navbar">
+          <div className="navbar-container">
+            <img src="./logo_white.svg" />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default NavBar;
