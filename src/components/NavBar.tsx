be82a4e63eff8dc8e6d8fef6f2d32130c0dbac07
Nazim-Yoldas Sesen <nazimsesen@gmail.com>
2023-10-18 19:14:21 -0400

    Added max-width to container

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -2,9 +2,19 @@ function NavBar() {
   return (
     <>
       <div className="page-padding">
-        <div className="Navbar">
+        <div className="navbar">
           <div className="navbar-container">
-            <img src="./logo_white.svg" />
+            <div className="nav-left">
+              <img className="img-logo" src="./logo_white.svg" />
+            </div>
+            <div className="nav-center">
+              <span className="navbar-menu-item">About</span>
+              <span className="navbar-menu-item">Services</span>
+              <span className="navbar-menu-item">Projects</span>
+            </div>
+            <div className="nav-right">
+              <span className="button">Contact</span>
+            </div>
           </div>
         </div>
       </div>
