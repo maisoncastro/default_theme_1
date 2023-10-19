45b3ae58448db75f875a0bbb439b793aae245a63
Nazim-Yoldas Sesen <nazimsesen@gmail.com>
2023-10-19 14:42:52 -0400

    Hero Section component added

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import NavBar from "./components/NavBar";
+import Hero from "./components/Hero";
 
 function App() {
   return (
     <>
       <div className="main_container">
         <NavBar />
+        <Hero />
       </div>
     </>
   );
---
+++ src/components/Hero.tsx
@@ -0,0 +1,11 @@
+function Hero() {
+  return (
+    <>
+      <div className="page-padding">
+        <div className="main-hero"></div>
+      </div>
+    </>
+  );
+}
+
+export default Hero;
