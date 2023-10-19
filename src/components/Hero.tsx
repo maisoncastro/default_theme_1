14ad24c7ed5d362ed86428e644e210c603cb9c96
Nazim-Yoldas Sesen <nazimsesen@gmail.com>
2023-10-19 16:00:17 -0400

    Hero Image added with Title and Subtitle

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -2,7 +2,24 @@ function Hero() {
   return (
     <>
       <div className="page-padding">
-        <div className="main-hero"></div>
+        <div className="main-hero-container">
+          <div className="hero-title">
+            <h1 className="">
+              Crafting Digital Excellence,
+              <br />
+              One Pixel at a Time
+            </h1>
+            <span className="hero-subtitle">
+              Unveiling a New Realm of Online Engagement Through Custom-Crafted
+              Web Designs
+            </span>
+          </div>
+          <div className="hero-image-container">
+            <div className="hero-image-wrapper">
+              <img className="hero-image" src="/placeholder-gradient.jpg/" />
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
