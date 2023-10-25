3d372ce8a7c38a5dce0fa76a71586d23942c2aec
Nazim-Yoldas Sesen <nazimsesen@gmail.com>
2023-10-25 14:08:04 -0400

    Added Contact Us at bottom of page

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import NavBar from "./components/NavBar";
 import Hero from "./components/Hero";
 import Services from "./components/Services";
 import Projects from "./components/Projects";
+import ContactCard from "./components/ContactCard";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Hero />
         <Services />
         <Projects />
+        <ContactCard />
       </div>
     </>
   );
---
+++ src/components/ContactCard.tsx
@@ -0,0 +1,25 @@
+function ContactCard() {
+  return (
+    <>
+      <div className="page-padding">
+        <div className="main-container-contact-us">
+          <div className="contact-us">
+            <img src="/contact-logo.svg" alt="" />
+            <div className="contact-us-bottom">
+              <div className="contact-us-content">
+                <h4>Contact</h4>
+                <h2>
+                  Ready to create your
+                  <br /> next standout project?
+                </h2>
+              </div>
+              <span className="button">Get In Touch</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default ContactCard;
