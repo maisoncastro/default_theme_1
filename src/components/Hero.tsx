f26d66e327f340955bdeab5fede90982f4c37834
Nazim-Yoldas Sesen <nazimsesen@gmail.com>
2023-10-25 15:58:38 -0400

    Added Animation aspect

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -2,25 +2,40 @@ import Marquee from "react-fast-marquee";
 import { motion } from "framer-motion";
 
 function Hero() {
+  const containerVariants = {
+    hidden: { opacity: 0 },
+    visible: {
+      opacity: 1,
+      transition: {
+        staggerChildren: 0.5,
+      },
+    },
+  };
+
+  const itemVariants = {
+    hidden: { y: 50, opacity: 0 },
+    visible: { y: 0, opacity: 1, transition: { duration: 2 } },
+  };
+
   return (
     <>
       <div className="page-padding">
         <div className="main-hero-container">
           <motion.div
             className="hero-title"
-            initial={{ y: 50, opacity: 0 }}
-            animate={{ y: 0, opacity: 1 }}
-            transition={{ duration: 2 }}
+            variants={containerVariants}
+            initial="hidden"
+            animate="visible"
           >
-            <h1 className="">
+            <motion.h1 variants={itemVariants}>
               Crafting Digital Excellence,
               <br />
               One Pixel at a Time
-            </h1>
-            <span className="hero-subtitle">
+            </motion.h1>
+            <motion.span className="hero-subtitle" variants={itemVariants}>
               Unveiling a New Realm of Online Engagement Through Custom-Crafted
               Web Designs
-            </span>
+            </motion.span>
           </motion.div>
           <motion.div
             className="hero-image-container"
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,3 +1,5 @@
+import { motion } from "framer-motion";
+
 function NavBar() {
   return (
     <>
