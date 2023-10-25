71a9b9dbad7cf48ac2f46c834c66f57bc4265cd9
Nazim-Yoldas Sesen <nazimsesen@gmail.com>
2023-10-25 15:47:48 -0400

    Added animation effects for Hero, and Title

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,11 +1,17 @@
 import Marquee from "react-fast-marquee";
+import { motion } from "framer-motion";
 
 function Hero() {
   return (
     <>
       <div className="page-padding">
         <div className="main-hero-container">
-          <div className="hero-title">
+          <motion.div
+            className="hero-title"
+            initial={{ y: 50, opacity: 0 }}
+            animate={{ y: 0, opacity: 1 }}
+            transition={{ duration: 2 }}
+          >
             <h1 className="">
               Crafting Digital Excellence,
               <br />
@@ -15,21 +21,26 @@ function Hero() {
               Unveiling a New Realm of Online Engagement Through Custom-Crafted
               Web Designs
             </span>
-          </div>
-          <div className="hero-image-container">
+          </motion.div>
+          <motion.div
+            className="hero-image-container"
+            initial={{ y: -50, opacity: 0 }}
+            animate={{ y: 0, opacity: 1 }}
+            transition={{ duration: 2 }}
+          >
             <div className="hero-image-wrapper">
               <img className="hero-image" src="/placeholder-gradient.jpg/" />
+            </div>{" "}
+            <div className="hero-banner">
+              <Marquee className="marquee" loop={0} autoFill={true}>
+                <span>+++</span>
+                <span>DIGITAL AGENCY BASED IN MONTREAL</span>
+                <span>WEB DEVELOPMENT</span>
+                <span>UX/UI DESIGN</span>
+                <span>BRANDING</span>
+              </Marquee>
             </div>
-          </div>
-          <div className="hero-banner">
-            <Marquee className="marquee" loop={0} autoFill={true}>
-              <span>+++</span>
-              <span>DIGITAL AGENCY BASED IN MONTREAL</span>
-              <span>WEB DEVELOPMENT</span>
-              <span>UX/UI DESIGN</span>
-              <span>BRANDING</span>
-            </Marquee>
-          </div>
+          </motion.div>
         </div>
       </div>
     </>
